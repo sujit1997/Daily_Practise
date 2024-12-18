b62a3a5140111c6c6f91a8bba66e43a49dbebe41
sujit1997 <sujitmanmode1997@gmail.com>
2024-12-18 14:14:35 +0530

    Todays commit

---
+++ src/main/java/R_18_12/SMPS8_1.java
@@ -0,0 +1,26 @@
+package R_18_12;
+
+class Employee {
+    int salary;
+    String name;
+
+    public int getSalary(){
+      return salary;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String n) {
+        name = n;
+    }
+}
+public class SMPS8_1 {
+    public static void main(String[] args) {
+        System.out.println("This is our custom class ");
+        Employee sujit = new Employee();
+        sujit.setName("Sujit Manmode");
+        sujit.salary = 30;
+        System.out.println(sujit.getName());
+        System.out.println(sujit.getSalary());
+    }
+}
