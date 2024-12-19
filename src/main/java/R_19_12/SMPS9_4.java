3b70fbd1eb01c3492bdaf41aa6d92b8906c3670b
sujit1997 <sujitmanmode1997@gmail.com>
2024-12-19 09:06:23 +0530

    Todays commit

---
+++ src/main/java/R_19_12/SMPS9_4.java
@@ -0,0 +1,28 @@
+package R_19_12;
+
+import java.awt.*;
+
+class Rectangle{
+private int length;
+private int breadth;
+
+    public Rectangle(int length, int breadth) {
+        this.length = 4;
+        this.breadth = 5;
+    }
+
+    public int getBreadth() {
+        return breadth;
+    }
+
+    public int getLength() {
+        return length;
+    }
+}
+public class SMPS9_4 {
+    public static void main(String[] args) {
+    Rectangle r = new Rectangle(12,15);
+        System.out.println(r.getLength());
+        System.out.println(r.getBreadth());
+    }
+}
