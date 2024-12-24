f0b143ab6a105619e727c1ff10dc788581d6240a
sujit1997 <sujitmanmode1997@gmail.com>
2024-12-24 09:12:21 +0530

    Todays commit

---
+++ src/main/java/R_24_12/SMPS13_1.java
@@ -0,0 +1,23 @@
+package R_24_12;
+class practise13 extends Thread{
+        public void run(){
+            while (true){
+                System.out.println("Good morning ");
+            }
+        }
+ }
+class practise13b extends Thread{
+    public void run(){
+        while (true){
+            System.out.println("Welcome");
+        }
+    }
+}
+public class SMPS13_1 {
+    public static void main(String[] args) {
+    practise13 p1 = new practise13();
+    practise13b p2 = new practise13b();
+    p1.start();
+    p2.start();
+    }
+}
