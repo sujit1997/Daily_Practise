c356196e36f9394ea3f4fa4495390fef25f62e1f
sujit1997 <sujitmanmode1997@gmail.com>
2024-12-25 15:08:37 +0530

    Todays commit

--- src/main/java/R_16_12/SMPS5_9.java
+++ src/main/java/R_16_12/SMPS5_9.java
@@ -1,18 +1,7 @@
-import java.util.Scanner;
-
 public class SMPS5_9 {
-    public int fact2(int num2){
-        if(num2 <=1){
-            return 1;
-        }
-        return num2 * fact2(num2-1);
-    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number to get its factorial using recursion ");
-        int num2 = sc.nextInt();
-        SMPS5_9 obj = new SMPS5_9();
-        int result = obj.fact2(num2);
-        System.out.println(result);
+        for(int i=1;i<=10;i++){
+            System.out.println("5");
+        }
     }
 }
