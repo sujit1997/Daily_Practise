3c463916a3d11542ca4e2b15cf115fe64e6305b8
sujit1997 <sujitmanmode1997@gmail.com>
2024-12-26 19:54:41 +0530

    Todays commit

---
+++ src/main/java/R_26_12/SMPS5_12.java
@@ -0,0 +1,12 @@
+package R_26_12;
+
+public class SMPS5_12 {
+    public static void main(String[] args) {
+        int n = 8;
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += n * i;
+        }
+        System.out.println(sum);
+    }
+}
