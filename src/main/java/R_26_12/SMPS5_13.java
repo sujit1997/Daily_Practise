3f23893644dcc4112b08e42c10260a39e4b2c246
sujit1997 <sujitmanmode1997@gmail.com>
2024-12-26 19:58:28 +0530

    Todays commit

---
+++ src/main/java/R_26_12/SMPS5_13.java
@@ -0,0 +1,10 @@
+package R_26_12;
+
+public class SMPS5_13 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n; i++){
+            System.out.println(i);
+        }
+    }
+}
