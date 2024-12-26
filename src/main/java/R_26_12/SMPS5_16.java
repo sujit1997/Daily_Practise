942282a5da84dde2490a9535c8c120cc5e444eb0
sujit1997 <sujitmanmode1997@gmail.com>
2024-12-26 20:15:41 +0530

    Todays commit

---
+++ src/main/java/R_26_12/SMPS5_16.java
@@ -0,0 +1,15 @@
+package R_26_12;
+
+public class SMPS5_16 {
+    public static void main(String[] args) {
+        int a= 6;
+        int b = 8;
+        int c;
+        c = a;
+        a = b;
+        b=c;
+        System.out.println("The value of a is: " +a);
+        System.out.println("The value of b is : " +b);
+    }
+
+}
