4bc762a4860b26b1180395c738b65f38958b9283
sujit1997 <sujitmanmode1997@gmail.com>
2024-12-29 19:37:14 +0530

    Todays commit

---
+++ src/main/java/R_29_12/SMPS5_27.java
@@ -0,0 +1,22 @@
+package R_29_12;
+
+public class SMPS5_27 {
+    public static void main(String[] args) {
+        int rows = 4; // Number of rows
+
+        for (int i = rows; i >= 1; i--) {
+            // Print leading spaces
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line
+            System.out.println();
+        }
+    }
+}
