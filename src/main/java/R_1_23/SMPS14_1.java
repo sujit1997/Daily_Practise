51dddc6addc9a96fdde52aad0f00ab8c21e9a791
sujit <sujitmanmode1997@gmail.com>
2025-01-23 10:46:03 +0530

    first commit

---
+++ src/main/java/R_1_23/SMPS14_1.java
@@ -0,0 +1,12 @@
+package R_1_23;
+
+public class SMPS14_1 {
+    public static void main(String[] args) {
+        //int a = 7 -- syntax error cause semicolon is not present
+
+        // int age = 78;
+        // int yearborn = 2000-78; // logical error you need to minus from current year
+
+        //System.out.println(6/0); // Runtime eroor
+    }
+}
