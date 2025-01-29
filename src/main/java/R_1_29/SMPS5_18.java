462be521cf8964b75ca35c3d99930841290b6b10
sujit <sujitmanmode1997@gmail.com>
2025-01-29 13:21:02 +0530

    first commit

--- src/main/java/R_1_29/SMPS5_18.java
+++ src/main/java/R_1_29/SMPS5_18.java
@@ -2,9 +2,9 @@ package R_1_29;
 
 public class SMPS5_18 {
     public static void main(String[] args) {
-        int num = 9;
+        int num = 10;
         int temp =0;
-        for(int i=2;i<=num-1;i++){
+        for(int i=2;i<=num/2;i++){
             if(num%i==0){
                 temp = temp+1;
             }
