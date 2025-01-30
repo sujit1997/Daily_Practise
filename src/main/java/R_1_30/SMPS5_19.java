fd112fc7bee21183b7a5d87d9ab04213a2cbc79b
sujit <sujitmanmode1997@gmail.com>
2025-01-30 21:36:44 +0530

    first commit

--- src/main/java/R_1_30/SMPS5_19.java
+++ src/main/java/R_1_30/SMPS5_19.java
@@ -1,21 +1,12 @@
 package R_1_30;
-import java.util.Scanner;
+
 public class SMPS5_19 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number");
-        int num = sc.nextInt();
-        boolean isItPrime = true;
-        for(int i=2;i<=num-1;i++){
-            if(num%i==0){
-                isItPrime = false;
+
+        for(int i=2;i<=100;i++){
+            for (int j=2;j<=j-1;j++){
+                //if(j%i)
             }
-        }
-        if(isItPrime){
-            System.out.println("number is prime");
-        }
-        else {
-            System.out.println("number is not prime");
         }
     }
 }
