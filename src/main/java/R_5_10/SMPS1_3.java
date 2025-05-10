f1f7d0785f2a9e84400bb5b6348258c785bb0f3c
sujit <sujitmanmode1997@gmail.com>
2025-05-10 20:53:31 +0530

    first commit

---
+++ src/main/java/R_5_10/SMPS1_3.java
@@ -0,0 +1,19 @@
+package R_5_10;
+import java.util.Scanner;
+public class SMPS1_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a marks of physics : ");
+        int a = sc.nextInt();
+        System.out.println("Enter marks of chemistry : ");
+        int b = sc.nextInt();
+        System.out.println("Enter marks of mathematics : ");
+        int c = sc.nextInt();
+        int total = a+b+c;
+        float per = total/300.0f*100;
+        System.out.format("Your percentage is %.2f" ,per);
+        System.out.println();
+        float cgpa = per/10.0f;
+        System.out.format("Your percentage is %.1f" ,cgpa);
+    }
+}
---
+++ src/main/java/R_5_10/SMPS2_1.java
@@ -0,0 +1,7 @@
+package R_5_10;
+
+public class SMPS2_1 {
+    public static void main(String[] args) {
+        
+    }
+}
