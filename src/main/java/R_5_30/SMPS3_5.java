8a2c8dea05db5d7fb075633d2f580716174ca8a8
SUJIT <sujitmanmode1997@gmail.com>
2025-05-30 09:13:11 +0530

    first commit

--- src/main/java/R_5_30/SMPS3_5.java
+++ src/main/java/R_5_30/SMPS3_5.java
@@ -2,6 +2,6 @@ package R_5_30;
 
 public class SMPS3_5 {
     public static void main(String[] args) {
-        System.out.println("Dear Harry,\n\tThis java course is very nice\n\tThan\ ");
+        System.out.println("Dear Harry,\n\tThis java course is very nice\n\tThanks ");
     }
 }
