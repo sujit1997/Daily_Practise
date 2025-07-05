a6685accb97b6782f66ef6e7fc7b9de958a01876
SUJIT <sujitmanmode1997@gmail.com>
2025-07-05 06:45:12 +0530

    first commit

---
+++ src/main/java/R_7_4/SMPS1_11.java
@@ -0,0 +1,9 @@
+package R_7_4;
+
+public class SMPS1_11 {
+    public static void main(String[] args) {
+    char letter = 'a';
+    int ascii = (int)letter;
+        System.out.println("the Ascii value of " +letter+ " is "+ascii);
+    }
+}
