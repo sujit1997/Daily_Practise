adeadff9159334cac5e30c9e0ae46e082a4700db
SUJIT <sujitmanmode1997@gmail.com>
2025-07-13 09:28:15 +0530

    first commit

---
+++ src/main/java/R_7_12/SMPS3_6_rr.java
@@ -0,0 +1,9 @@
+package R_7_12;
+
+public class SMPS3_6_rr {
+    public static void main(String[] args) {
+        String sentance = "This sentance contain number of word";
+        String[] num = sentance.trim().split("\\s+");
+        System.out.println(num.length);
+    }
+}
---
+++ src/main/java/R_7_12/SMPS3_8_r.java
@@ -0,0 +1,8 @@
+package R_7_12;
+
+public class SMPS3_8_r {
+    public static void main(String[] args) {
+
+
+    }
+}
