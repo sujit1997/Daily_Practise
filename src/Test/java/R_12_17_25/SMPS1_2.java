e834bd97e0a463676b3c0690973276a8222be45e
sujit <sujitmanmode1997@gmail.com>
2025-12-17 09:11:25 +0530

    todays commit

---
+++ src/Test/java/R_12_17_25/SMPS1_2.java
@@ -0,0 +1,11 @@
+package R_12_17_25;
+
+public class SMPS1_2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int d = a+b+c;
+        System.out.println(d);
+    }
+}
