4668baa94638fc42eeb75e49c1126fc8b0aeda14
sujit <sujitmanmode1997@gmail.com>
2026-01-01 09:30:00 +0530

    todays commit

---
+++ src/Test/java/R_01_01_26/SMPS2_2.java
@@ -0,0 +1,13 @@
+package R_01_01_26;
+
+import java.util.Scanner;
+
+public class SMPS2_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your grade : ");
+        char grade = sc.next().charAt(0);
+        char newgrade = (char) (grade + 8);
+        System.out.println(newgrade);
+    }
+}
