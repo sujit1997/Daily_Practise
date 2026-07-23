74fe84760c9607f20baf1995fd381a1b1ea6adda
Sujit <sujitmanmode1997@gmail.com>
2026-07-23 11:43:00 -0400

    23/7/26

---
+++ src/Test/java/R_23_07_26/SMPS1_2.java
@@ -0,0 +1,11 @@
+package R_23_07_26;
+
+public class SMPS1_2 {
+    public static void main(String[] args) {
+        int a= 5;
+        int b =6;
+        int c;
+        c= a+b;
+        System.out.println(c);
+    }
+}
